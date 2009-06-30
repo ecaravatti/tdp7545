51b197b2e1dd55f18d81029f10768b786089e9dc
dpisaturo <dpisaturo@c8a76e56-1a3f-11de-82b5-194b17410922>
2009-06-30 01:27:49 +0000

    Arreglé un problema de concurrencia que había con el patrón observer.

--- WiiDesktop/WiiDesktop/Common/Subject.cs
+++ WiiDesktop/WiiDesktop/Common/Subject.cs
@@ -8,28 +8,82 @@ namespace WiiDesktop.Common
     public abstract class Subject
     {
         private ArrayList observers = new ArrayList();
-        private ArrayList pendingToRemove = new ArrayList();
+        private ArrayList pendingTasks = new ArrayList();
 
         public void AddObserver(Observer observer)
         {
-            observers.Add(observer);
+            pendingTasks.Add(new Task(TaskType.ADD, observer));
         }
 
         public void RemoveObserver(Observer observer)
         {
-            pendingToRemove.Add(observer);
+            pendingTasks.Add(new Task(TaskType.REMOVE, observer));
         }
 
         public void Notify()
         {
-            foreach (Observer observer in pendingToRemove)
+            if (pendingTasks.Count > 0)
             {
-                observers.Remove(observer);
+                foreach (Task task in pendingTasks)
+                {
+                    if (task.Type.Equals(TaskType.ADD))
+                    {
+                        observers.Add(task.Observer);
+                    }
+                    else if (task.Type.Equals(TaskType.REMOVE))
+                    {
+                        observers.Remove(task.Observer);
+                    }
+                }
+
+                pendingTasks.Clear();
             }
 
             foreach (Observer observer in observers)
             {
                 observer.Update(this);
+            }
+        }
+    }
+
+    enum TaskType
+    {
+        ADD,
+        REMOVE
+    }
+
+    class Task
+    {
+        private TaskType type;
+        private Observer observer;
+
+        public Task(TaskType type, Observer observer)
+        {
+            this.type = type;
+            this.observer = observer;
+        }
+
+        public TaskType Type
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                type = value;
+            }
+        }
+
+        public Observer Observer
+        {
+            get
+            {
+                return observer;
+            }
+            set
+            {
+                observer = value;
             }
         }
     }
