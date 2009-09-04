b80bfee76883442101659a3b3fc41f9a99d4f01b
rdz.maria.laura <rdz.maria.laura@c8a76e56-1a3f-11de-82b5-194b17410922>
2009-09-04 21:13:39 +0000

    Subo la Gscreen, estaria bueno cambiar el fondo de las imagenes de los gestos, que no estan transparentes.

--- trunk/WiiDesktop/WiiDesktop/View/GScreen.Designer.cs
+++ trunk/WiiDesktop/WiiDesktop/View/GScreen.Designer.cs
@@ -1,3 +1,11 @@
+using System.ComponentModel;
+using MouseGestures;
+using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using WiiDesktop.Domain.Gestures;
+using System.Drawing;
 namespace WiiDesktop.View
 {
     partial class GScreen
@@ -6,6 +14,15 @@ namespace WiiDesktop.View
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.PictureBox pbGesture;
+        private MouseGestures.MouseGestures mouseGesturesTest;
+        private System.Windows.Forms.Timer timerReset;
+        private PictureBox[] mouseGestureDisplay;
+        private MouseGesture[] mouseGesturesList = new MouseGesture[16]{MouseGesture.Up, MouseGesture.Right ,MouseGesture.Down, 
+        MouseGesture.Left, MouseGesture.UpRight, MouseGesture.UpDown, MouseGesture.UpLeft, MouseGesture.RightUp, MouseGesture.RightDown, 
+        MouseGesture.RightLeft, MouseGesture.DownUp, MouseGesture.DownRight, MouseGesture.DownLeft, MouseGesture.LeftUp,
+        MouseGesture.LeftRight, MouseGesture.LeftDown};
+
 
         /// <summary>
         /// Clean up any resources being used.
@@ -20,6 +37,7 @@ namespace WiiDesktop.View
             base.Dispose(disposing);
         }
 
+
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -29,10 +47,100 @@ namespace WiiDesktop.View
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.pbGesture = new System.Windows.Forms.PictureBox();
+            this.timerReset = new System.Windows.Forms.Timer(this.components);
+            this.mouseGesturesTest = new MouseGestures.MouseGestures(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGesture)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbGesture
+            // 
+            this.pbGesture.Location = new System.Drawing.Point(20, 28);
+            this.pbGesture.Name = "pbGesture";
+            this.pbGesture.Size = new System.Drawing.Size(46, 50);
+            this.pbGesture.TabIndex = 0;
+            this.pbGesture.TabStop = false;
+            // 
+            // timerReset
+            // 
+            this.timerReset.Interval = 500;
+            this.timerReset.Tick += new System.EventHandler(this.timerReset_Tick);
+            // 
+            // mouseGesturesTest
+            // 
+            this.mouseGesturesTest.Gesture += new MouseGestures.MouseGestures.GestureHandler(this.mouseGestures_Gesture);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pbGesture);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(260, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Recognized gesture";
+            // 
+            // GScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 125);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "GScreen";
             this.Text = "GScreen";
+            ((System.ComponentModel.ISupportInitialize)(this.pbGesture)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
+
         #endregion
+
+        private void mouseGestures_Gesture(object sender, MouseGestureEventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo.FileName = GestureConfiguration.Instance.ResolveCommand(e.Gesture);
+                process.Start();
+            }
+            catch (Exception)
+            {
+                // No hay comando configurado para el gesto detectado
+            }
+            foreach (PictureBox pb in mouseGestureDisplay)
+            {
+                if ((MouseGesture)pb.Tag == e.Gesture)
+                {
+                    //load image from the resources
+                    pbGesture.Image = new Bitmap(Assembly.GetExecutingAssembly().GetManifestResourceStream(
+              getResourceName(e.Gesture, true)));
+                    timerReset.Start();
+                    return;
+                }
+            }
+        }
+
+        private void timerReset_Tick(object sender, EventArgs e)
+        {
+            //resets images to normal
+            timerReset.Stop();
+        }
+
+        //Build full name of the resource
+        private string getResourceName(MouseGesture mg, bool activeImage)
+        {
+            string gestureName = Enum.GetName(typeof(MouseGesture), (MouseGesture)mg);
+            string returnValue = "WiiDesktop.Resources.Images." + gestureName + ".png";
+            return returnValue;
+        }
+
+        private GroupBox groupBox1;
+
+
+
     }
 }
--- trunk/WiiDesktop/WiiDesktop/View/GScreen.cs
+++ trunk/WiiDesktop/WiiDesktop/View/GScreen.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
 
 namespace WiiDesktop.View
 {
@@ -12,7 +14,27 @@ namespace WiiDesktop.View
     {
         public GScreen()
         {
+            InitializePictureBoxList();
             InitializeComponent();
+            //InitializePbConfigImage();
         }
+
+
+        private void InitializePictureBoxList()
+        {
+            mouseGestureDisplay = new PictureBox[16];
+            for (int i = 0; i < 16; i++)
+            {
+                mouseGestureDisplay[i] = new PictureBox();
+                mouseGestureDisplay[i].Tag = mouseGesturesList[i];
+            }
+        }
+
+        private void InitializePbConfigImage()
+        {
+            this.pbGesture.Image = new Bitmap(Assembly.GetExecutingAssembly().GetManifestResourceStream(
+            "WiiDesktop.Resources.Images.configIcon.png"));
+        }
+     
     }
 }
